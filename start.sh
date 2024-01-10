d98c4733417e9899841a6d11a0c25d51bb65534a
ThePhaseless <kukubaorch@gmail.com>
2024-01-10 01:57:59 +0100

    Add error handling and retry mechanism in start.sh script

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@
 while true; do
 	OUTPUT=$(php ./index.php)
 	echo "$OUTPUT"
+	# Check if the output contains the string "LimitExceeded"
 	case "$OUTPUT" in
 	*LimitExceeded*) break ;;
+	# If "Will retry after" is found, sleep for 500 seconds
+	*Will\ retry\ after*) sleep 500 ;;
 	esac
 done
